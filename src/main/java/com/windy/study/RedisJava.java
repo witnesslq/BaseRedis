b58ff9c391021596edacc5d441c85c73071feab7
windy <530650681@qq.com>
2016-08-05 09:29:04 +0800

    handle with string

---
+++ src/main/java/com/windy/study/RedisJava.java
@@ -0,0 +1,18 @@
+package com.windy.study;
+
+import redis.clients.jedis.Jedis;
+
+/**
+ * 描述.
+ * @author zhangjunyong
+ * @version V1.0  2016/8/4 12:56
+ */
+public class RedisJava {
+    public static void main(String[] args) {
+        //Connecting to Redis Server on localhost
+        Jedis jedis = new Jedis("localhost");
+        System.out.println("Connection to server successfully.");
+        //check whether server is running or not
+        System.out.println("Server is running:" + jedis.ping());
+    }
+}
---
+++ src/main/java/com/windy/study/RedisStringJava.java
@@ -0,0 +1,19 @@
+package com.windy.study;
+
+import redis.clients.jedis.Jedis;
+
+/**
+ * 描述.
+ * @author zhangjunyong
+ * @version V1.0  2016/8/4 14:19
+ */
+public class RedisStringJava {
+    public static void main(String[] args) {
+        //Connecting to Redis Server on localhost
+        Jedis jedis = new Jedis("localhost");
+        System.out.println("Connection to server successfully.");
+        //set the data in redis string
+        jedis.set("name", "xiaoming");
+        System.out.println("the string name in the redis :" + jedis.get("name"));
+    }
+}
